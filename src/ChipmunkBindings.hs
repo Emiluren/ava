5818a3c38a072365fc185a5d54ae5a6b6ff04b2c
Emil Segerbäck <emilsegerback@gmail.com>
2017-10-12 14:04:34 +0200

    Refactor chipmunk bindings

--- src/ChipmunkBindings.hs
+++ src/ChipmunkBindings.hs
@@ -7,7 +7,7 @@ import Data.Monoid ((<>))
 import Data.StateVar (StateVar, makeStateVar)
 import Foreign.Marshal.Alloc (alloca, free, malloc)
 import Foreign.Ptr (Ptr, FunPtr, freeHaskellFunPtr)
-import Foreign.Storable (peek, poke)
+import Foreign.Storable (peek, poke, Storable)
 import qualified Language.C.Inline as C
 import System.IO.Unsafe (unsafePerformIO)
 
@@ -27,24 +27,27 @@ allCategories :: CpBitmask
 {-# NOINLINE allCategories #-}
 allCategories = unsafePerformIO [C.exp| unsigned int { CP_ALL_CATEGORIES } |]
 
+storableGetterSetter :: Storable a => (Ptr a -> IO ()) -> (Ptr a -> IO ()) -> StateVar a
+storableGetterSetter getAction setAction = makeStateVar getter setter where
+    getter = alloca (\ptr -> getAction ptr >> peek ptr)
+    setter x = alloca (\ptr -> poke ptr x >> setAction ptr)
+
 rotate :: Vector -> Vector -> Vector
-rotate vec1 vec2 = unsafePerformIO rot
-    where
-        rot = do
-            [v1, v2, v3] <- replicateM 3 malloc
-            poke v1 vec1
-            poke v2 vec2
-            [C.exp| void { *$(cpVect* v3) = cpvrotate(*$(cpVect* v1), *$(cpVect* v2)) } |]
-            res <- peek v3
-            mapM_ free [v1, v2, v3]
-            return res
+rotate vec1 vec2 = unsafePerformIO rot where
+    rot = do
+        [v1, v2, v3] <- replicateM 3 malloc
+        poke v1 vec1
+        poke v2 vec2
+        [C.exp| void { *$(cpVect* v3) = cpvrotate(*$(cpVect* v1), *$(cpVect* v2)) } |]
+        res <- peek v3
+        mapM_ free [v1, v2, v3]
+        return res
 
 fromAngle :: CpFloat -> Vector
-fromAngle a = unsafePerformIO $ alloca fromA
-    where
-        fromA vec = do
-            [C.exp| void { *$(cpVect* vec) = cpvforangle($(double a)) } |]
-            peek vec
+fromAngle a = unsafePerformIO $ alloca fromA where
+    fromA vec = do
+        [C.exp| void { *$(cpVect* vec) = cpvforangle($(double a)) } |]
+        peek vec
 
 newSpace :: IO (Ptr Space)
 newSpace = [C.exp| cpSpace* { cpSpaceNew() } |]
@@ -58,35 +61,25 @@ step space dt = [C.exp| void { cpSpaceStep($(cpSpace* space), $(double dt)) } |]
 addCollisionHandler :: Ptr Space -> CpCollisionType -> CpCollisionType -> Handler -> IO ()
 addCollisionHandler space colType1 colType2 (Handler begh preh posth seph) =
     [C.block| void {
-                cpCollisionBeginFunc beginFunc = $(cpBool (*begh)(cpArbiter*, cpSpace*, void*));
-                cpCollisionPreSolveFunc preSolveFunc = $(cpBool (*preh)(cpArbiter*, cpSpace*, void*));
-                cpCollisionPostSolveFunc postSolveFunc = $(void (*posth)(cpArbiter*, cpSpace*, void*));
-                cpCollisionSeparateFunc separateFunc = $(void (*seph)(cpArbiter*, cpSpace*, void*));
+        cpCollisionBeginFunc beginFunc = $(cpBool (*begh)(cpArbiter*, cpSpace*, void*));
+        cpCollisionPreSolveFunc preSolveFunc = $(cpBool (*preh)(cpArbiter*, cpSpace*, void*));
+        cpCollisionPostSolveFunc postSolveFunc = $(void (*posth)(cpArbiter*, cpSpace*, void*));
+        cpCollisionSeparateFunc separateFunc = $(void (*seph)(cpArbiter*, cpSpace*, void*));
 
-                cpCollisionHandler* handler = cpSpaceAddCollisionHandler($(cpSpace* space),
-                                                                         $(unsigned int colType1),
-                                                                         $(unsigned int colType2));
+        cpCollisionHandler* handler = cpSpaceAddCollisionHandler($(cpSpace* space),
+                                                                 $(unsigned int colType1),
+                                                                 $(unsigned int colType2));
 
-                if (beginFunc != NULL) handler->beginFunc = beginFunc;
-                if (preSolveFunc != NULL) handler->preSolveFunc = preSolveFunc;
-                if (postSolveFunc != NULL) handler->postSolveFunc = postSolveFunc;
-                if (separateFunc != NULL) handler->separateFunc = separateFunc;
-            }
-        |]
+        if (beginFunc != NULL) handler->beginFunc = beginFunc;
+        if (preSolveFunc != NULL) handler->preSolveFunc = preSolveFunc;
+        if (postSolveFunc != NULL) handler->postSolveFunc = postSolveFunc;
+        if (separateFunc != NULL) handler->separateFunc = separateFunc;
+    } |]
 
 gravity :: Ptr Space -> StateVar Vector
-gravity space = makeStateVar getter setter
-    where
-        getter =
-            let getVec vec = do
-                    [C.exp| void { *$(cpVect* vec) = cpSpaceGetGravity($(cpSpace* space)) } |]
-                    peek vec
-            in alloca getVec
-        setter v =
-            let setVec vec = do
-                    poke vec v
-                    [C.exp| void { cpSpaceSetGravity($(cpSpace* space), *$(cpVect* vec)) } |]
-            in alloca setVec
+gravity space = storableGetterSetter getter setter where
+    getter vec = [C.exp| void { *$(cpVect* vec) = cpSpaceGetGravity($(cpSpace* space)) } |]
+    setter vec = [C.exp| void { cpSpaceSetGravity($(cpSpace* space), *$(cpVect* vec)) } |]
 
 newShape :: Ptr Body -> ShapeType -> IO (Ptr Shape)
 newShape body (LineSegment a b radius) =
@@ -112,42 +105,29 @@ newShape body (Polygon vect radius) =
                                          $(double radius)) } |]
 
 friction :: Ptr Shape -> StateVar CpFloat
-friction shape = makeStateVar getter setter
-    where
-        getter = [C.exp| double { cpShapeGetFriction($(cpShape* shape))} |]
-        setter v = [C.exp| void { cpShapeSetFriction($(cpShape* shape), $(double v))} |]
+friction shape = makeStateVar getter setter where
+    getter = [C.exp| double { cpShapeGetFriction($(cpShape* shape))} |]
+    setter v = [C.exp| void { cpShapeSetFriction($(cpShape* shape), $(double v))} |]
 
 elasticity :: Ptr Shape -> StateVar CpFloat
-elasticity shape = makeStateVar getter setter
-    where
-        getter = [C.exp| double { cpShapeGetElasticity($(cpShape* shape))} |]
-        setter v = [C.exp| void { cpShapeSetElasticity($(cpShape* shape), $(double v))} |]
+elasticity shape = makeStateVar getter setter where
+    getter = [C.exp| double { cpShapeGetElasticity($(cpShape* shape))} |]
+    setter v = [C.exp| void { cpShapeSetElasticity($(cpShape* shape), $(double v))} |]
 
 collisionType :: Ptr Shape -> StateVar CpCollisionType
-collisionType shape = makeStateVar getter setter
-    where
-        getter = [C.exp| unsigned int { cpShapeGetCollisionType($(cpShape* shape)) } |]
-        setter v = [C.exp| void { cpShapeSetCollisionType($(cpShape* shape), $(unsigned int v)) } |]
+collisionType shape = makeStateVar getter setter where
+    getter = [C.exp| unsigned int { cpShapeGetCollisionType($(cpShape* shape)) } |]
+    setter v = [C.exp| void { cpShapeSetCollisionType($(cpShape* shape), $(unsigned int v)) } |]
 
 surfaceVel :: Ptr Shape -> StateVar Vector
-surfaceVel shape = makeStateVar getter setter
-    where
-        getter =
-            let getVec vec = do
-                    [C.exp| void { *$(cpVect* vec) = cpShapeGetSurfaceVelocity($(cpShape* shape)) } |]
-                    peek vec
-            in alloca getVec
-        setter v =
-            let setVec vec = do
-                    poke vec v
-                    [C.exp| void { cpShapeSetSurfaceVelocity($(cpShape* shape), *$(cpVect* vec)) } |]
-            in alloca setVec
+surfaceVel shape = storableGetterSetter getter setter where
+    getter vec = [C.exp| void { *$(cpVect* vec) = cpShapeGetSurfaceVelocity($(cpShape* shape)) } |]
+    setter vec = [C.exp| void { cpShapeSetSurfaceVelocity($(cpShape* shape), *$(cpVect* vec)) } |]
 
 shapeBody :: Ptr Shape -> StateVar (Ptr Body)
-shapeBody shape = makeStateVar getter setter
-    where
-        getter = [C.exp| cpBody* { cpShapeGetBody($(cpShape* shape)) } |]
-        setter b = [C.exp| void { cpShapeSetBody($(cpShape* shape), $(cpBody* b)) } |]
+shapeBody shape = makeStateVar getter setter where
+    getter = [C.exp| cpBody* { cpShapeGetBody($(cpShape* shape)) } |]
+    setter b = [C.exp| void { cpShapeSetBody($(cpShape* shape), $(cpBody* b)) } |]
 
 newBody :: CpFloat -> CpFloat -> IO (Ptr Body)
 newBody m i = [C.exp| cpBody* { cpBodyNew($(double m), $(double i)) } |]
@@ -156,42 +136,24 @@ freeBody :: Ptr Body -> IO ()
 freeBody body = [C.exp| void { cpBodyFree($(cpBody* body)) } |]
 
 position :: Ptr Body -> StateVar Vector
-position body = makeStateVar getter setter
-    where
-        getter =
-            let getVec vec = do
-                    [C.exp| void { *$(cpVect* vec) = cpBodyGetPosition($(cpBody* body)) } |]
-                    peek vec
-            in alloca getVec
-        setter v =
-            let setVec vec = do
-                    poke vec v
-                    [C.exp| void { cpBodySetPosition($(cpBody* body), *$(cpVect* vec)) } |]
-            in alloca setVec
+position body = storableGetterSetter getter setter where
+    getter vec = [C.exp| void { *$(cpVect* vec) = cpBodyGetPosition($(cpBody* body)) } |]
+    setter vec = [C.exp| void { cpBodySetPosition($(cpBody* body), *$(cpVect* vec)) } |]
 
 force :: Ptr Body -> StateVar Vector
-force body = makeStateVar getter setter
-    where
-        getter =
-            let getVec vec = do
-                    [C.exp| void { *$(cpVect* vec) = cpBodyGetForce($(cpBody* body)) } |]
-                    peek vec
-            in alloca getVec
-        setter v =
-            let setVec vec = do
-                    poke vec v
-                    [C.exp| void { cpBodySetForce($(cpBody* body), *$(cpVect* vec)) } |]
-            in alloca setVec
+force body = storableGetterSetter getter setter where
+    getter vec = [C.exp| void { *$(cpVect* vec) = cpBodyGetForce($(cpBody* body)) } |]
+    setter vec = [C.exp| void { cpBodySetForce($(cpBody* body), *$(cpVect* vec)) } |]
 
 angle :: Ptr Body -> StateVar CpFloat
-angle body = makeStateVar getter setter
-    where
-        getter = [C.exp| double { cpBodyGetAngle($(cpBody* body))} |]
-        setter v = [C.exp| void { cpBodySetAngle($(cpBody* body), $(double v))} |]
+angle body = makeStateVar getter setter where
+    getter = [C.exp| double { cpBodyGetAngle($(cpBody* body))} |]
+    setter v = [C.exp| void { cpBodySetAngle($(cpBody* body), $(double v))} |]
 
 bodyEachArbiter :: Ptr Body -> FunPtr BodyArbiterIteratorFun -> IO ()
-bodyEachArbiter body func = [C.exp| void { cpBodyEachArbiter($(cpBody* body),
-                                                             $(void (*func)(cpBody*, cpArbiter*, void*)), NULL) } |]
+bodyEachArbiter body func =
+    [C.exp| void { cpBodyEachArbiter($(cpBody* body),
+                                     $(void (*func)(cpBody*, cpArbiter*, void*)), NULL) } |]
 
 bodyEachArbiterList :: Ptr Body -> IO [Ptr Arbiter]
 bodyEachArbiterList body = do
@@ -202,8 +164,6 @@ bodyEachArbiterList body = do
     freeHaskellFunPtr arbiterCallback
     return arbiters
 
-
-
 spaceGetStaticBody :: Ptr Space -> IO (Ptr Body)
 spaceGetStaticBody space = [C.exp| cpBody* { cpSpaceGetStaticBody($(cpSpace* space)) } |]
 
@@ -212,16 +172,6 @@ spaceAddShape space shape = [C.exp| void { cpSpaceAddShape($(cpSpace* space), $(
 
 spaceAddBody :: Ptr Space -> Ptr Body -> IO ()
 spaceAddBody space body = [C.exp| void { cpSpaceAddBody($(cpSpace* space), $(cpBody* body)) } |]
-
--- spaceSegmentQuery :: Ptr Space -> Vector -> Vector -> CpFloat -> ShapeFilter -> FunPtr SpaceSegmentQueryFun -> IO ()
--- spaceSegmentQuery space start end radius filter func = do
---     startPtr <- malloc
---     endPtr <- malloc
---     poke startPtr start
---     poke endPtr end
---     [C.exp| void {} |]
---     free startPtr
---     free endPtr
 
 spaceSegmentQueryFirst :: Ptr Space -> Vector -> Vector -> CpFloat -> ShapeFilter -> IO SegmentQueryInfo
 spaceSegmentQueryFirst space start end radius (ShapeFilter group categories mask) = do
